ff73fc010fd5d189c37dd8fdaa26628d52ba8242
kodiitulip <kodii.tulip@gmail.com>
2024-09-12 16:20:02 -0300

    Player Scene & Attack Resource

---
+++ scripts/resources/attack_resource.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name AttackResource
+
+@export var damage: float = 1.0
+@export_range(0.0, 1.0, 0.01,'allow_greater') var critical_chance: float = 0.0:
+    set(crit):
+        critical_chance = max(crit, 0.0)
+@export_range(0.0, 1.0, 0.01,'allow_greater') var critical_multiplier: float = 1.2:
+    set(crit_mult):
+        critical_multiplier = max(crit_mult, 0.0)
